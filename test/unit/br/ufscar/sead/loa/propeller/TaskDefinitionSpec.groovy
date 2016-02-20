b378ec0f92137a583add72afd0072d2e72cb387b
Matheus <matheus.frndes@gmail.com>
2016-02-19 23:58:24 -0200

    Add tests for TaskDefinition

---
+++ test/unit/br/ufscar/sead/loa/propeller/TaskDefinitionSpec.groovy
@@ -0,0 +1,47 @@
+package br.ufscar.sead.loa.propeller
+
+import br.ufscar.sead.loa.propeller.domain.TaskDefinition
+import org.bson.Document
+import spock.lang.Specification
+
+/**
+ * Created by matheus on 2/19/16.
+ * https://github.com/matheuss
+ */
+class TaskDefinitionSpec extends Specification {
+
+    def setupSpec() {
+        Propeller.instance.init([dbName: 'propeller', wipeDb: true])
+    }
+
+    def cleanupSpec() {
+        Propeller.instance.ds.DB.dropDatabase()
+    }
+
+    def "create a task without any output"() {
+        def definition
+
+        setup:
+        def doc
+        doc = Document.parse(new File('test/resources/forca.json').text).get('tasks').first() as Document
+        doc.remove('outputs')
+        definition = new TaskDefinition(doc)
+
+        expect:
+        !definition.validate()
+    }
+
+    def "create a task without an invalid output"() {
+        def definition
+
+        setup:
+        def doc
+        doc = Document.parse(new File('test/resources/forca.json').text).get('tasks').first() as Document
+        doc = Document.parse(doc.toJson().replace('perguntas.json', ''))
+        definition = new TaskDefinition(doc)
+
+        expect:
+        !definition.validate()
+    }
+
+}
